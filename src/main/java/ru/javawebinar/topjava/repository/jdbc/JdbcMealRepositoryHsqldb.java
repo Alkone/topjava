7f32def6fe4ef4c15ad9de9e721f0ff20ab0844f
alkone <devalkone@gmail.com>
2019-11-15 14:40:44 +0300

    HW05 is done

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHsqldb.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("hsqldb")
+public class JdbcMealRepositoryHsqldb extends JdbcMealRepository {
+
+    public JdbcMealRepositoryHsqldb(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+
+}
